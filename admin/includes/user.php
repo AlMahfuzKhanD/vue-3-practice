fcd710c6c04ad131d6fe670dcd1a9abbbf55f73b
mahfuz380 <almahfuz380@gmail.com>
2019-03-09 23:51:57 +0600

    user class updated

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -9,9 +9,12 @@
 
 class User{
     public static function find_all_users(){
-        global $database;
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
+        //global $database;
+        //$result_set = $database->query("SELECT * FROM users");
+        //return $result_set;
+        
+        return self::find_this_query("SELECT * FROM users");
+        
 
 
     }
@@ -28,5 +31,11 @@ class User{
     	$result_user_id = $database->query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
     	$find_user = mysqli_fetch_array($result_user_id);
     	return $find_user;
+    }
+
+    public static function find_this_query($sql){
+    	global $database;
+    	$result_set = $database->query($sql);
+    	return $result_set;
     }
 }
