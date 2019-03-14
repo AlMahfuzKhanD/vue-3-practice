24bd434fd133f2729245a13ce9f1476ab94c1902
mahfuz380 <almahfuz380@gmail.com>
2019-03-14 20:16:24 +0600

    autoload created

---
+++ admin/includes/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function classAutoLoader($class){  //autoloader is used to check if class declered or not.
+$class = strtolower($class);
+$the_path = "includes/{$class}.php";
+
+if(is_file($the_path) && !class_exists($class)){
+	include $the_path;
+}
+
+} // end of autoload
+
+spl_autoload_register('classAutoLoader');
+
+?>
