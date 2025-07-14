943b6e747296ed04c5b9b0a7a2323a1fad26eba7
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-07-14 23:32:47 +0600

    vue 3 reactivity practice done

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Counter: {{ counter }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+function increment() {
+  counter.value++;
+}
+</script>
