cb3b8a992314df0fbbd9efe5244a5bc3843f6c7b
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-08-30 15:47:46 +0600

    watch effect done

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,17 @@
     <!-- <Day1User /> -->
     <!-- <Day1Practice /> -->
     <!-- <Day2Computed /> -->
-    <Day2Watcher />
+    <!-- <Day2Watcher /> -->
+    <Day2WatchEffect />
   </div>
 </template>
 
 <script setup>
 // import Day2BMI from './components/Day2BMI.vue'
 import Day1Counter from './components/Day1Counter.vue'
-import Day1User from './components/Day1User.vue'
-import Day1Practice from './components/Day1Practice.vue'
-import Day2Computed from './components/Day2Computed.vue'
-import Day2Watcher from './components/Day2Watcher.vue'
+// import Day1User from './components/Day1User.vue'
+// import Day1Practice from './components/Day1Practice.vue'
+// import Day2Computed from './components/Day2Computed.vue'
+// import Day2Watcher from './components/Day2Watcher.vue'
+import Day2WatchEffect from './components/Day2WatchEffect.vue'
 </script>
---
+++ src/components/Day2WatchEffect.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+const name = ref('Al Mahfuz')
+const age = ref(25)
+
+watchEffect(() => {
+    console.log(`Name is ${name.value} and age is ${age.value}`)
+})
+</script>
+<template>
+    <p>Name: <input v-model="name" /></p>
+    <p>Age <input v-model.number="age" /></p>
+</template>
