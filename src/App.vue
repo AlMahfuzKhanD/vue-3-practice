e0dccec38a48905097bc80f823c7cee80ad122da
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-08-31 11:26:20 +0600

    slot done

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,12 @@
     <!-- <Day2BMI /> -->
     <!-- <Day3Props name="Alice" :age="30" />
     <Day3Props name="Mahfuz" :age="22" /> -->
-    <Day3Emit  @update="handleUpdate"/>
-    <p>Latest count: {{ latestCount }}</p>
+    <!-- <Day3Emit  @update="handleUpdate"/>
+    <p>Latest count: {{ latestCount }}</p> -->
+    <Day3Slot>
+      <p>This is some content inside the card.</p>
+      <button>Click Me</button>
+    </Day3Slot>
 
   </div>
 </template>
@@ -28,10 +32,12 @@
 // import Day2WatchEffect from './components/Day2WatchEffect.vue'
 // import Day2BMI from './components/Day2BMI.vue'
 // import Day3Props from './components/Day3Props.vue'
-import Day3Emit from './components/Day3Emit.vue';
-import { ref } from 'vue';
-const latestCount = ref(0);
-function handleUpdate(newValue){
-  latestCount.value = newValue;
-}
+// import Day3Emit from './components/Day3Emit.vue';
+// import { ref } from 'vue';
+// const latestCount = ref(0);
+// function handleUpdate(newValue){
+//   latestCount.value = newValue;
+// }
+
+import Day3Slot from './components/Day3Slot.vue'
 </script>
---
+++ src/components/Day3Slot.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Day 3: Slots</h2>
+        <slot></slot>
+    </div>
+</template>
