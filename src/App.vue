ae99053dd57af50054938bd58b062730fa8cc904
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-08-31 10:38:40 +0600

    Day3 props done

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,26 @@
     <h1>Vue 3 Practice Playground</h1>
 
     <!-- Swap this line depending on what you want to test -->
-    <Day1Counter />
+    <!-- <Day1Counter /> -->
     <!-- <Day1User /> -->
     <!-- <Day1Practice /> -->
     <!-- <Day2Computed /> -->
     <!-- <Day2Watcher /> -->
     <!-- <Day2WatchEffect /> -->
-    <Day2BMI />
+    <!-- <Day2BMI /> -->
+    <Day3Props name="Alice" :age="30" />
+    <Day3Props name="Mahfuz" :age="22" />
   </div>
 </template>
 
 <script setup>
 // import Day2BMI from './components/Day2BMI.vue'
-import Day1Counter from './components/Day1Counter.vue'
+// import Day1Counter from './components/Day1Counter.vue'
 // import Day1User from './components/Day1User.vue'
 // import Day1Practice from './components/Day1Practice.vue'
 // import Day2Computed from './components/Day2Computed.vue'
 // import Day2Watcher from './components/Day2Watcher.vue'
 // import Day2WatchEffect from './components/Day2WatchEffect.vue'
-import Day2BMI from './components/Day2BMI.vue'
+// import Day2BMI from './components/Day2BMI.vue'
+import Day3Props from './components/Day3Props.vue'
 </script>
---
+++ src/components/Day3Props.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <h2>{{ name }}</h2>
+    <p>Age: {{ age }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  age: Number
+})
+</script>
